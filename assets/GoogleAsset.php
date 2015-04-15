e22ff864ac32b56f8d4fe698dfb8c282a6f51f00
RSmirnov <rsmirnov@nm.ru>
2015-04-16 02:18:31 +0300

    Working with auth forms, discovering JS

---
+++ assets/GoogleAsset.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RSmirnov
+ * Date: 16.04.2015
+ * Time: 2:00
+ */
+
+namespace app\assets;
+
+
+class GoogleAsset {
+
+}
---
+++ assets/GooglePlaceAutoload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RSmirnov
+ * Date: 16.04.2015
+ * Time: 2:03
+ */
+
+namespace app\assets;
+
+
+class GooglePlaceAutoload {
+
+}
